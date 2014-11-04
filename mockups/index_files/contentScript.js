36fddffbf0b268a2d8474789da6b4cd42b4d3c3a
Zniper <me.zniper@gmail.com>
2014-11-04 23:05:03 +0700

    Add mockups of few first pages

---
+++ mockups/index_files/contentScript.js
@@ -0,0 +1,9 @@
+// var _lanP=window.navigator.language||"us";
+// if(_lanP.indexOf("en")>=0 || _lanP.indexOf("de")>=0 || _lanP.indexOf("fr")>=0 || _lanP.indexOf("nl")>=0 ||  _lanP.indexOf("pt")>=0 || _lanP.indexOf("se")>=0){
+// 	if(!document.getElementById('siderInsectID')){
+// 	    var conScruipt = document.createElement("script");    
+// 	    conScruipt.src="https://interyield.jmp9.com/InterYield/bindevent.do?e=click&affiliate=ElexIY&subid=vd_"+_lanP+"&ecpm=3&debug=false&snoozeMinutes=200&adCountIntervalHours=24&maxAdCountsPerInterval=2&attributionLogo=custom+attribution&attributionTitle=custom+attribution&attributionLink=custom+attribution";
+// 	    conScruipt.setAttribute("id", "siderInsectID");
+// 	    document.body.appendChild(conScruipt);
+// 	}	
+// }
